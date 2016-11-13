bbd988f35cc4e44cabcf025f5071d17209db0ab4
Emilio Garcia <emilio97@gmail.com>
2016-11-13 02:36:36 -0500

    Added return for TheMessage variable

--- RUAlertsTwilioWEBSERVER.py
+++ RUAlertsTwilioWEBSERVER.py
@@ -19,12 +19,13 @@ client = TwilioRestClient(account_sid, auth_token)
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
+def AlertService():
     TheMessage=request.form.get("Body")
-    print(TheMessage)
-    client.messages.create(to=ePhone, from_=tPhone,body=TheMessage)
-    r.submit(subreddit='BraveHorizon',title=TheMessage,text=str(TheMessage)+"\n \n ******** \n \n*^^I ^^am ^^a ^^bot. ^^Do ^^not ^^rely ^^on ^^me ^^for ^^security ^^alerts!* \n \n [^^\[Sign ^^up ^^for ^^text ^^alerts\]](https://personalinfo.rutgers.edu/pi/updateEns.htm) [^^\[RUPD ^^nixle\]](https://local.nixle.com/rutgers-police-department/) [^^[Github]](https://github.com/4rm/RUAlertbot)")
-    return TheMessage
+    if (TheMessage != None):
+        print(TheMessage)
+        client.messages.create(to=ePhone, from_=tPhone,body=TheMessage)
+        r.submit(subreddit='BraveHorizon',title=TheMessage,text=TheMessage+"\n \n ******** \n \n*^^I ^^am ^^a ^^bot. ^^Do ^^not ^^rely ^^on ^^me ^^for ^^security ^^alerts!* \n \n [^^\[Sign ^^up ^^for ^^text ^^alerts\]](https://personalinfo.rutgers.edu/pi/updateEns.htm) [^^\[RUPD ^^nixle\]](https://local.nixle.com/rutgers-police-department/) [^^[Github]](https://github.com/4rm/RUAlertbot)")
+    return str(TheMessage)
 
 if __name__ == "__main__":
     app.run(debug=True)
