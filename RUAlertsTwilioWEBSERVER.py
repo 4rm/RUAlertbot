26b03ba36788dc086853615b8c1f39ca26a23b29
Emilio Garcia <emilio97@gmail.com>
2016-11-18 10:33:24 -0500

    Added favicon support

--- RUAlertsTwilioWEBSERVER.py
+++ RUAlertsTwilioWEBSERVER.py
@@ -2,10 +2,12 @@ import RUAlertsTwilioWEBSERVER
 import twilio.twiml
 import time
 import praw
+import os
 
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, send_from_directory
 from twilio.rest import TwilioRestClient
 from passwords import *
+from twilio import twiml
 
 def login():
     r = praw.Reddit(app_ua)
@@ -23,9 +25,15 @@ def AlertService():
     TheMessage=request.form.get("Body")
     if (TheMessage != None):
         print(TheMessage)
-        client.messages.create(to=ePhone, from_=tPhone,body=TheMessage)
-        r.submit(subreddit='BraveHorizon',title=TheMessage,text=TheMessage+"\n \n ******** \n \n*^^I ^^am ^^a ^^bot. ^^Do ^^not ^^rely ^^on ^^me ^^for ^^security ^^alerts!* \n \n [^^\[Sign ^^up ^^for ^^text ^^alerts\]](https://personalinfo.rutgers.edu/pi/updateEns.htm) [^^\[RUPD ^^nixle\]](https://local.nixle.com/rutgers-police-department/) [^^[Github]](https://github.com/4rm/RUAlertbot)")
+        client.messages.create(body=TheMessage,to=ePhone,from_=tPhone)
+        r.submit(subreddit='BraveHorizon',title=TheMessage,text=TheMessage+"\n \n ******** \n \n*^^I ^^am ^^a ^^bot. ^^Do ^^not ^^rely ^^on ^^me ^^for ^^security ^^alerts!$
     return str(TheMessage)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
+
