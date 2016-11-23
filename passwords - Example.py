1da87c8358eebb111474624c557323bde9e26e4f
Emilio Garcia <emilio97@gmail.com>
2016-11-23 15:20:28 -0500

    Update passwords - Example.py

--- passwords - Example.py
+++ passwords - Example.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-app_id = 'xxxxxxxxxxxxxxxx'
-app_secret = 'xxxxxxxxxxxxxxxx'
-app_uri = 'xxxxxxxxxxxxxxxx'
-app_scopes = 'xxxxxxxxxxxxxxxx'
-app_ua='xxxxxxxxxxxxxxxx'
-app_account_code = 'xxxxxxxxxxxxxxxx'
-app_refresh = 'xxxxxxxxxxxxxxxx'
-emailpass='xxxxxxxxxxxxxxxx'
-emailaddress='xxxxxxxxxxxxxxxx'
-=======
 app_id = 'xxxxx'
 app_secret = 'xxxxx'
 app_uri = 'xxxxx'
@@ -19,5 +8,3 @@ app_refresh = 'xxxxx'
 account_sid = "xxxxx"
 auth_token = "xxxxx"
 ePhone = "+1xxxxxxxxxx"
-tPhone = "+1xxxxxxxxxx"
->>>>>>> refs/remotes/origin/Twilio
