14561977224896abdf115f9921f8335265ca5090
Emilio Garcia <emilio97@gmail.com>
2016-11-25 18:43:18 -0500

    Added link to OAuth2 Guide

--- passwords - Example.py
+++ passwords - Example.py
@@ -1,3 +1,5 @@
+#Helpful guide for setting up OAuth2: https://www.reddit.com/r/GoldTesting/comments/3cm1p8/how_to_make_your_bot_use_oauth2/
+
 app_id = 'xxxxx'
 app_secret = 'xxxxx'
 app_uri = 'xxxxx'
