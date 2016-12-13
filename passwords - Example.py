ca2d28fa3633a12bba747a60e55f938345519be2
Emilio Garcia <emilio97@gmail.com>
2016-12-12 19:28:37 -0500

    Update passwords - Example.py

--- passwords - Example.py
+++ passwords - Example.py
@@ -2,11 +2,9 @@
 
 app_id = 'xxxxx'
 app_secret = 'xxxxx'
-app_uri = 'xxxxx'
-app_scopes = 'xxxxx'
 app_ua='xxxxx'
-app_account_code = 'xxxxx'
-app_refresh = 'xxxxx'
-account_sid = "xxxxx"
-auth_token = "xxxxx"
-ePhone = "+1xxxxxxxxxx"
+account_sid = 'xxxxx'
+auth_token = 'xxxxx'
+ePhone = '+1xxxxxxxxxx'
+username = 'xxxxx'
+password = 'xxxxx'
